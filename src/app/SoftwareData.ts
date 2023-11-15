dfb8d2c6dce51f0b6d152f1d57b0066bf3534031
Rick Vermeil <rickv85@gmail.com>
2023-11-15 10:37:58 -0700

    Update Software Data copy

--- src/app/SoftwareData.ts
+++ src/app/SoftwareData.ts
@@ -20,15 +20,15 @@ export const data = [
     ytLink: "https://www.youtube.com/embed/WvRA8F9EQsI",
     img: "",
     descFontStyle: "text-[#F6AE2D] text-lg",
-    description: `Fueled by the grit of a seasoned mountain biker and the curiosity of a budding software developer, I embarked on a journey to create an application that has the potential to revolutionize the bike maintenance experience. This idea struck me as I was rebuilding my mountain bike suspension. I was grappling with a problem that most mountain bikers are all too familiar with - recalling the last service date of their suspension and then trying to calculate the number of riding hours since then, a key piece of data used by suspension manufacturers to recommend service intervals.
+    description: `Fueled by the grit and determination of a competitive mountain biker, I embarked on a journey to create an application that has the potential to revolutionize the bike maintenance experience. This idea struck me as I was rebuilding my own mountain bike suspension. I was pondering a problem that most mountain bikers are all too familiar with - trying to recall the last service date of their suspension and then trying to make an educated guess on wether or not it was due for service or not. Typically, suspension manufacturers recommend the service intervals in an amount of hours ridden since the last rebuild was performed.
 
-    For the 3rd quarter final project at Turing's School of Software and Design, I took on the challenge of crafting an app that addresses this problem within a tight 6-day deadline. In this venture, I not only pushed my limits but also got my hands dirty with new technology such as OAuth2.0.
+    For the 3rd quarter final project at Turing's School of Software and Design, I took on the challenge of building an app that addresses this problem within a tight 6-day deadline on my own. In this venture, I not only pushed my limits but also got my hands dirty with some technology that was new to me, such as OAuth2.0. The success I achieved in building this application made me reflect on how far I had come as a developer. Not long before, I couldn't fathom building a complex application that solves a real-world problem, especially in such a short time frame.
     
-    Looking back, it's astonishing to realize how far I've come. Only a year ago, I couldn't fathom building a complex application that tackles a real-world problem in such a short time. Now, not only do I believe in my potential to create virtually anything given the time and resources, but I also see the potential that this app has to help my fellow mountain bikers. All components on a bicycle need to be serviced or replaced over time and a tool, like this one, that could remind a user of those intervals would be very useful for the cycling community at large.
+    Now, not only do I believe in my potential to create virtually anything given the time and resources, but I also see the potential that this app has to help my fellow mountain bikers. All components on a bicycle need to be serviced or replaced over time and a tool, like this one, that could remind a user of those intervals would be very useful for the cycling community at large.
     
-    Recently, as I hunted for my first developer role, I decided to enhance the app further and learn some new skills. I've developed a fully functional backend to house users' suspension data, eliminating the need for that data to be stored in local storage. This venture introduced me to the world of APIs and full CRUD functionality using Node.js, Express, and SQL. This app continues to motivate me to learn new skills and apply them through enhancing this product with new features.
+    Recently, while on the hunt for my first developer role, I decided to enhance the app further and learn some new skills. I've developed a fully functional backend using Node.js, Express, and SQL to house users' suspension data, eliminating the need for that data to be stored in local storage. This app continues to motivate me to learn new skills and apply them through enhancing this product with new features.
     
-    Please note, this application requires a Strava account and the tracking of mountain bike rides for accurate operation. For non-bikers, I invite you to enjoy the video above and welcome you to reach out for a personal demonstration.`,
+    Please note, this application requires a Strava account and the tracking of mountain bike rides for accurate operation. For those who do not mountain bike or do not use Strava in this way, I invite you to enjoy the video above and welcome you to reach out for a personal demonstration.`,
     linkLogoColor: "F26419",
     deployedLink: "https://www.ridereadybike.com",
     ghLink: "https://github.com/orgs/RideReady/repositories",
@@ -61,7 +61,7 @@ export const data = [
     
     This application is designed to allow a user to research US National Park campgrounds by a variety of options including by state, park name, campground name and their current location. The user is presented with search result options which they can browse and then select one for more information or add to their favorites which will appear on the home page. On the details page for a campground, they will receive vital information about the campground that will help them plan their trip.
 
-    The campground seeker can browse images of the selected campground, an interactive meet provided by Google Maps API, attributes of the campground and a review section that allows a user to submit their opinion of the campground as well as a photo which is stored in an Amazon S3 Bucket. Finally, the user can click links to either be navigated directly to the campground from their location or book a site on the NPS reservation system Recreation.gov.`,
+    The campground seeker can browse images of the selected campground, an interactive map provided by Google Maps API, attributes of the campground and a review section that allows a user to submit their opinion of the campground as well as a photo which is stored in an Amazon S3 Bucket. Finally, the user can click links to either be navigated directly to the campground from their location with Google Maps or book a site on the NPS reservation system, Recreation.gov.`,
     linkLogoColor: "F9A03F",
     deployedLink: "https://backcountrybookings.herokuapp.com/",
     ghLink: "https://github.com/Backcountry-Bookings/backcountry_fe",
@@ -90,9 +90,9 @@ export const data = [
 
     LavLink allows a user to search for public restrooms in their area that meet their criteria. Users can stipulate whether they need a wheelchair accessible bathroom, a unisex bathroom, or a restroom with a changing table. This ensures that trans people, caregivers, and people with mobility equipment know exactly where they need to go when they need to go!
     
-    LavLink users can use their current location to find restrooms in their immediate area, or plan ahead by entering a zip code to search. They are provided with a list of results along with a map showing each one. By clicking on a listed restroom or map marker, they are redirected to a page to view further information about the bathroom (address, map, comments, upvotes, etc.) and a way to leave the app to pull up directions to the restroom from their current location.
+    LavLink users can allow the app to use their current location to find restrooms in their immediate area, or plan ahead by entering a zip code to search. They are provided with a list of results along with a map showing each one. By clicking on a listed restroom or map marker, they are redirected to a page to view further information about the bathroom (address, map, comments, upvotes, etc.) and a way to leave the app to pull up directions to the restroom from their current location.
     
-    A mobile-first design means this app is designed to work on any phone, but has a responsive design for those with larger screen needs.`,
+    This app was designed "mobile-first" and is primarily intended for a user on a mobile device, but also features a responsive UI design for larger screens.`,
     linkLogoColor: "f6c28b",
     deployedLink: "https://lav-link-smithkirsten.vercel.app/",
     ghLink: "https://github.com/RickV85/lav-link",
@@ -105,9 +105,9 @@ export const data = [
     ytLink: "",
     img: "https://user-images.githubusercontent.com/109977562/218594737-f5ef70de-cdd0-4681-9204-b62db32136c6.gif",
     descFontStyle: "text-[#f0dfa3]",
-    description: `This was a two week, partner project assigned to students in Mod 3 of the Turing School of Software & Design's Front-End Engineering bootcamp. Our goal was to create a user-friendly application that displays all the movies in our collection along with their Funky Flix rating. The purpose of our application is to assist users in determining if a movie is worth watching by providing them with information about each movie.
+    description: `This was a two week, partner project assigned to students in Mod 3 of the Turing School of Software & Design's Front-End Engineering program. Our goal was to create a user-friendly application that displays all available movies available from a Turing created API along with their Funky Flix rating. The purpose of our application is to assist users in determining if a movie is worth watching by providing them with information about each movie, similar to "Rotten Tomatoes".
 
-    Our functionality was inspired by popular movie review websites like Rotten Tomatoes, and provides a comprehensive browsing experience where users can sort how the movies are displayed or search for a particular movie by name. By clicking on a movie, users are redirected to that movie's page, which includes the movie's trailer and additional details about the movie that may be of interest to the user, such as the movie's duration and genre.
+    Our functionality was inspired by popular movie viewing and reviewing web applications, and provides a comprehensive browsing experience where users can sort how the movies are displayed or search for a particular movie by name. By clicking on a movie, users are redirected to that movie's page, which includes the movie's trailer and additional details about the movie that may be of interest to the user, such as the movie's duration and genre.
 
     This project served as a valuable learning opportunity for us, as it was our first project that incorporated React, Cypress testing, and a multi-page user experience using the React Router.`,
     linkLogoColor: "f95738",
