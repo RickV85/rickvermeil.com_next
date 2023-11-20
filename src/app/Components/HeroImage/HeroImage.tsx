dc56fbbb35bdf559ea4ac1189f3f8074772b8cc3
Rick Vermeil <rickv85@gmail.com>
2023-11-20 10:06:33 -0700

    Format HeroImage

--- src/app/Components/HeroImage/HeroImage.tsx
+++ src/app/Components/HeroImage/HeroImage.tsx
@@ -7,14 +7,21 @@ interface Props {
   imgAlt: string;
 }
 
-export default function HeroImage({ imgIndex, currentImgIndex, imgUrl, imgAlt } : Props) {
+export default function HeroImage({
+  imgIndex,
+  currentImgIndex,
+  imgUrl,
+  imgAlt,
+}: Props) {
   return (
     <Image
       src={imgUrl}
       alt={imgAlt}
       height={1080}
       width={1920}
-      className={`absolute inset-0 bg-cover bg-center transition-opacity duration-1000 ${imgIndex === currentImgIndex ? "opacity-100" : "opacity-0"}`}
+      className={`absolute inset-0 bg-cover bg-center transition-opacity duration-1000 ${
+        imgIndex === currentImgIndex ? "opacity-100" : "opacity-0"
+      }`}
     ></Image>
   );
 }
