899aa0f05a42433a48a4736a3bdb57396e3ceb4f
Rick Vermeil <rickv85@gmail.com>
2023-11-20 10:05:09 -0700

    Add prettier TW plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,9 +9,15 @@ import Link from "next/link";
 
 function Home() {
   const heroImages = [
-    {url: "/Shelf-81.webp", alt: "Page author rock climbing on a sunny day"},
-    {url: "/DSC_0232.webp", alt: "Page author close up on a boat in Thailand"},
-    {url: "/Cropped_Keystone_Enduro_EvenFlow.webp", alt: "Page author downhill mountain bike racing"},
+    { url: "/Shelf-81.webp", alt: "Page author rock climbing on a sunny day" },
+    {
+      url: "/DSC_0232.webp",
+      alt: "Page author close up on a boat in Thailand",
+    },
+    {
+      url: "/Cropped_Keystone_Enduro_EvenFlow.webp",
+      alt: "Page author downhill mountain bike racing",
+    },
   ];
   const [heroImgIndex, setHeroImgIndex] = useState(0);
   const [isVisible, setIsVisible] = useState(false);
@@ -20,7 +26,7 @@ function Home() {
   useEffect(() => {
     const heroImgTimer = setInterval(() => {
       setHeroImgIndex((heroImgIndex + 1) % heroImages.length);
-    }, 6000);
+    }, 4000);
     return () => {
       clearInterval(heroImgTimer);
     };
@@ -38,7 +44,7 @@ function Home() {
         root: null,
         rootMargin: "0px",
         threshold: 0.1,
-      }
+      },
     );
     if (paragraphRef.current) {
       observer.observe(paragraphRef.current);
@@ -50,9 +56,9 @@ function Home() {
       <Nav section={"home"} />
       <main
         id="homeMain"
-        className="font-body flex flex-col items-center xl:m-auto xl:max-w-[1600px]"
+        className="flex flex-col items-center font-body xl:m-auto xl:max-w-[1600px]"
       >
-        <section className="relative flex items-center justify-center h-[30vh] sm:h-[44vh] md:h-[75vh] lg:h-[90vh] lg:max-h-[825px] w-screen xl:w-[1600px] xl:m-auto">
+        <section className="relative flex h-[30vh] w-screen items-center justify-center sm:h-[44vh] md:h-[75vh] lg:h-[90vh] xl:m-auto xl:w-[1600px]">
           {heroImages.map((image, i) => (
             <HeroImage
               key={`heroImg-${i}`}
@@ -62,31 +68,31 @@ function Home() {
               imgAlt={image.alt}
             />
           ))}
-          <div className="w-[225px] sm:w-[330px] md:w-[550px] -translate-x-16 sm:-translate-x-20 md:-translate-x-36 lg:-translate-x-64">
-            <h3 className=" text-[1.1rem] sm:text-[1.5rem] md:text-[2rem] lg:text-[3rem] sm:leading-[30px] md:leading-[40px] lg:leading-[60px]  font-semibold text-aqua  text-center [text-shadow:_2px_2px_5px_#000000] ">
+          <div className="w-[225px] -translate-x-16 sm:w-[330px] sm:-translate-x-20 md:w-[550px] md:-translate-x-36 lg:-translate-x-64">
+            <h3 className=" text-center text-[1.1rem] font-semibold text-aqua [text-shadow:_2px_2px_5px_#000000] sm:text-[1.5rem] sm:leading-[30px]  md:text-[2rem] md:leading-[40px]  lg:text-[3rem] lg:leading-[60px] ">
               Software Developer,
               <br />
               passionate adventurer,
               <br />
               driven by challenge
             </h3>
-            <div className=" w-full flex flex-col items-center py-4 ">
+            <div className=" flex w-full flex-col items-center py-4 ">
               <Link href={"/software"}>
-                <button className="w-fit lg:w-[16vw] lg:max-w-[250px] h-fit bg-teal/90 outline outline-1 outline-orange rounded-md text-aqua [text-shadow:_2px_2px_5px_#000000] text-xs sm:text-base lg:text-lg font-semibold p-2 ">
+                <button className="h-fit w-fit rounded-md bg-teal/90 p-2 text-xs font-semibold text-aqua outline outline-1 outline-orange [text-shadow:_2px_2px_5px_#000000] sm:text-base lg:w-[16vw] lg:max-w-[250px] lg:text-lg ">
                   Checkout my apps!
                 </button>
               </Link>
             </div>
           </div>
         </section>
-        <section className="flex flex-col items-center h-fit xl:max-w-[1600px]">
+        <section className="flex h-fit flex-col items-center xl:max-w-[1600px]">
           <p
-            className={`text-darkBlue font-light text-sm sm:text-base md:text-lg leading-loose whitespace-pre-line text-center w-inherit lg:my-6`}
+            className={`w-inherit whitespace-pre-line text-center text-sm font-light leading-loose text-darkBlue sm:text-base md:text-lg lg:my-6`}
           >
             {`Hello and welcome! When I'm not pursuing one of my passions in the outdoors, you'll find me designing and brining user-centric web applications to life. I love pushing my limits on a rock face, down a mountain bike trail and in software development. I believe that the same focus and determination required to conquer a challenging climb or trail translates seamlessly into the world of software development.`}
           </p>
-          <div className="bg-teal h-fit w-inherit flex flex-col-reverse mdlg:flex-row items-center ">
-            <div className="w-full m-4 sm:w-[60vw] md:max-w-sm md:min-w-[16rem] md:m-8">
+          <div className="w-inherit flex h-fit flex-col-reverse items-center bg-teal mdlg:flex-row ">
+            <div className="m-4 w-full sm:w-[60vw] md:m-8 md:min-w-[16rem] md:max-w-sm">
               <Image
                 src={"/crestprofilepic_webres.webp"}
                 alt="Rick Vermeil head shot"
@@ -96,10 +102,10 @@ function Home() {
                 priority={true}
               />
             </div>
-            <article className="text-beige text-sm md:text-base whitespace-pre-line ">
+            <article className="whitespace-pre-line text-sm text-beige md:text-base ">
               <p
                 ref={paragraphRef}
-                className={`p-6 transition-all duration-700 ease-in-out transform ${
+                className={`transform p-6 transition-all duration-700 ease-in-out ${
                   isVisible
                     ? "translate-y-0 opacity-100"
                     : "translate-y-4 opacity-0"
