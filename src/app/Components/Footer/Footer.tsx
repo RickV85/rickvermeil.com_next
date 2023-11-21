17968f8ce2c1c244c1acc43ad3af49daeffb6fe6
Rick Vermeil <rickv85@gmail.com>
2023-11-20 17:10:48 -0700

    Extend header and footer beyond 1600px for larger screens

--- src/app/Components/Footer/Footer.tsx
+++ src/app/Components/Footer/Footer.tsx
@@ -2,19 +2,21 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="flex h-fit w-screen max-w-[1600px] flex-col items-center justify-center bg-darkBlue py-2 ">
-      <Link
-        className="my-2 w-fit rounded-md bg-orange px-12 py-2 text-center"
-        href="mailto:rickv85@gmail.com"
-        target="_blank"
-        rel="noreferrer"
-      >
-        Email me
-      </Link>
-      <p className="text-center text-sm text-beige ">
-        {`If the above link doesn't work, please send me an email at
+    <footer className="bg-darkBlue w-screen flex flex-col items-center">
+      <div className="flex flex-col items-center h-fit w-screen max-w-[1600px] justify-center py-2">
+        <Link
+          className="my-2 w-fit rounded-md bg-orange px-12 py-2 text-center"
+          href="mailto:rickv85@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Email me
+        </Link>
+        <p className="text-center text-sm text-beige ">
+          {`If the above link doesn't work, please send me an email at
         rickv85(at)gmail.com`}
-      </p>
+        </p>
+      </div>
     </footer>
   );
 }
