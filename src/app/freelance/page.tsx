562cbaa8404d5a50ea03b2f5ca28a00625307956
Rick Vermeil <rickv85@gmail.com>
2023-11-26 10:27:23 -0700

    Style hero section, add responsiveness

--- src/app/freelance/page.tsx
+++ src/app/freelance/page.tsx
@@ -10,7 +10,7 @@ export default function Freelance() {
       <Nav section="freelance" />
       <main className="relative flex w-screen flex-col items-center ">
         <section className="flex flex-col items-center font-vws xl:max-w-[1600px]">
-          <div className="relative h-[45vh] w-screen sm:h-[60vh] mdlg:h-[75vh] xl:max-w-[1600px]">
+          <div className="relative h-[45vh] w-screen sm:h-[60vh] mdlg:h-[75vh] lg:h-[85vh] xl:max-w-[1600px]">
             <Image
               src={"/Laptop_in_mtn_landscape.webp"}
               alt="MacBook Pr in a Rocky Mountain landscape"
@@ -19,10 +19,10 @@ export default function Freelance() {
               className="object-cover"
             />
           </div>
-          <div className="absolute h-full flex flex-col items-center justify-between">
+          <div className="absolute flex h-full flex-col items-center justify-between">
             <div
               id="vwsLogoContainer"
-              className="mt-4 flex w-32 flex-col items-center justify-center sm:w-40 md:w-52"
+              className="mt-4 flex w-32 flex-col items-center justify-center sm:w-40 md:w-52 mdlg:w-64 "
             >
               <Image
                 src={"/logo-no-background.png"}
@@ -32,12 +32,15 @@ export default function Freelance() {
                 height={935}
                 className="shadow-[0px_0px_4px_3px_#ffffff94]"
               />
-              <h2 className="w-max text-center text-base font-semibold text-aqua drop-shadow-[1px_2px_3px_black] sm:text-lg ">
+              <h2 className="w-max text-center text-lg font-semibold text-aqua drop-shadow-[1px_2px_3px_black] md:text-2xl mdlg:text-3xl">
                 Vermeil Web Solutions
               </h2>
             </div>
-            <div className="text-center text-aqua drop-shadow-[1px_2px_3px_black] ">
-              <p className="text-sm sm:text-base">
+            {/* background-color: #2a2a2a40;
+    padding: 5px;
+    border-radius: 5px; */}
+            <div className="text-center  bg-[#2a2a2a8e] p-1 rounded-md ">
+              <p className="text-aqua drop-shadow-[1px_2px_3px_black] text-sm sm:text-base md:text-xl mdlg:text-2xl">
                 {"Elevating Your Apps to New Heights"}
               </p>
             </div>
